10f63105f9333e034ed7b76ac7a7ad52e2c273f9
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-07-02 13:16:10 +0100

    Update glm_Rt_wrap.R

--- R/glm_Rt_wrap.R
+++ R/glm_Rt_wrap.R
@@ -8,6 +8,7 @@
 #' @export
 glm_Rt_wrap <- function(I_incid, si_distr, t_window, overlap){
   
+  
   t_max <- nrow(I_incid)
   # time window
   t_start <- seq(2, t_max-(t_window-1),by = 1)
@@ -21,15 +22,23 @@ glm_Rt_wrap <- function(I_incid, si_distr, t_window, overlap){
                            incidence = rowSums(I_incid),
                            Oi = EpiEstim::overall_infectivity(incid = I_incid,
                                                               si_distr =  si_distr))
-  
+  # correct incidence when Oi is 0 -> move case as imported
+  f <- which(data_infer$Oi ==0)
+  if(length(f)>0){
+    data_infer$Oi[f] <- NA
+  }
   
   data_infer$log_Oi <- log(data_infer$Oi)
   
   # make fix factor for time windows
-  tw <- c(NA, rep(1:length(t_start),each = t_window[i]))
+  tw <- c(NA, rep(1:length(t_start),each = t_window))
   if(overlap==FALSE){
     data_infer$tw <- as.character(c(tw, rep(max(tw, na.rm=TRUE)+1,
                                             nrow(data_infer)-length(tw))))
+    f <- which(is.na(data_infer$Oi))
+    if(length(f)>0){
+      data_infer$tw[f] <- NA
+    }
   }else{
     # overlapping
     temp <- data_infer[1,]
@@ -39,6 +48,11 @@ glm_Rt_wrap <- function(I_incid, si_distr, t_window, overlap){
     }
     temp$tw <- as.character(tw)
     data_infer <- temp
+    
+    f <- which(is.na(data_infer$Oi))
+    if(length(f)>0){
+      data_infer$tw[f] <- NA
+    }
   }
   
   # coefficient in the above is equivalent to logI = log(Rt)+log(OI) -> Rt = exp(coeff)
@@ -62,5 +76,7 @@ glm_Rt_wrap <- function(I_incid, si_distr, t_window, overlap){
   
   res <- list(config = list(t_window, overlap, t_start, t_end), 
               Rt = data_infer[,c( "t","Mean","low_Quantile","high_Quantile")])
+
   return(res)
+  
 }
