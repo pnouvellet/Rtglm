8021559e2d21e4e2c590336c03a9b9174b73a78b
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-07-03 23:37:11 +0100

    Update gam_Rt_wrap.R

--- R/gam_Rt_wrap.R
+++ R/gam_Rt_wrap.R
@@ -6,17 +6,17 @@
 #'              Including the expected data type.
 #'
 #' @export
-gam_Rt_wrap <- function(I_incid, si_distr, t_window, overlap){
+gam_Rt_wrap <- function(I_incid, si_distr){
   
-  overlap <- FALSE
-  
-  t_max <- nrow(I_incid)
-  # time window
-  t_start <- seq(2, t_max-(t_window-1),by = 1)
-  if (overlap == FALSE) {
-    t_start <- t_start[seq(1,length(t_start),by = t_window)]
-  }
-  t_end <- t_start + (t_window - 1)    
+  # overlap <- FALSE
+  # 
+  # t_max <- nrow(I_incid)
+  # # time window
+  # t_start <- seq(2, t_max-(t_window-1),by = 1)
+  # if (overlap == FALSE) {
+  #   t_start <- t_start[seq(1,length(t_start),by = t_window)]
+  # }
+  # t_end <- t_start + (t_window - 1)    
   
   # reframe data
   data_infer <- data.frame(t = seq(1,nrow(I_incid)), 
@@ -31,25 +31,25 @@ gam_Rt_wrap <- function(I_incid, si_distr, t_window, overlap){
   
   data_infer$log_Oi <- log(data_infer$Oi)
   
-  # make fix factor for time windows
-  tw <- c(NA, rep(1:length(t_start),each = t_window))
-  if(overlap==FALSE){
-    data_infer$tw <- as.character(c(tw, rep(max(tw, na.rm=TRUE)+1,
-                                            nrow(data_infer)-length(tw))))
-  }else{ # overlapping
-    temp <- data_infer[1,]
-    for(k in 1:length(t_start)){
-      temp <- rbind(temp,
-                    data_infer[t_start[k]:t_end[k],])
-    }
-    temp$tw <- as.character(tw)
-    data_infer <- temp
-    
-  }
-  f <- which(is.na(data_infer$Oi))
-  if(length(f)>0){
-    data_infer$tw[f] <- NA
-  }
+  # # make fix factor for time windows
+  # tw <- c(NA, rep(1:length(t_start),each = t_window))
+  # if(overlap==FALSE){
+  #   data_infer$tw <- as.character(c(tw, rep(max(tw, na.rm=TRUE)+1,
+  #                                           nrow(data_infer)-length(tw))))
+  # }else{ # overlapping
+  #   temp <- data_infer[1,]
+  #   for(k in 1:length(t_start)){
+  #     temp <- rbind(temp,
+  #                   data_infer[t_start[k]:t_end[k],])
+  #   }
+  #   temp$tw <- as.character(tw)
+  #   data_infer <- temp
+  #   
+  # }
+  # f <- which(is.na(data_infer$Oi))
+  # if(length(f)>0){
+  #   data_infer$tw[f] <- NA
+  # }
   
   # coefficient in the above is equivalent to logI = log(Rt)+log(OI) -> Rt = exp(coeff)
   k_basis <- 5
