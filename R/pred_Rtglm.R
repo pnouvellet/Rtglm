5c2da75200d5699fcd9d8232fa9956b832a89d51
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-07-12 09:55:00 +0100

    Update pred_Rtglm.R

--- R/pred_Rtglm.R
+++ R/pred_Rtglm.R
@@ -8,23 +8,24 @@
 #' @export
 #' 
 pred_Rtglm <- function(model, newdata, overlap = FALSE){
-  pred <- mgcv::predict.gam(object = poi_glm, 
-                            newdata = data_infer,
+  
+  pred <- mgcv::predict.gam(object = model, 
+                            newdata = newdata,
                             type = 'link', 
                             se.fit = TRUE)
   
   # storing values to df
-  data_infer$Mean <- exp(pred$fit) / data_infer$Oi
-  data_infer$low_Quantile <- exp((pred$fit) - 1.96*pred$se.fit) / data_infer$Oi
-  data_infer$high_Quantile <- exp((pred$fit) + 1.96*pred$se.fit) / data_infer$Oi
-  data_infer$Std <- pred$se.fit
+  newdata$Mean <- exp(pred$fit) / newdata$Oi
+  newdata$low_Quantile <- exp((pred$fit) - 1.96*pred$se.fit) / newdata$Oi
+  newdata$high_Quantile <- exp((pred$fit) + 1.96*pred$se.fit) / newdata$Oi
+  newdata$Std <- pred$se.fit
   
   # readjust data.frame to return the center of time window
   if(overlap==TRUE){
-    data_infer <- data_infer[seq(1+ceiling(t_window/2),
-                                 nrow(data_infer), 
+    newdata <- newdata[seq(1+ceiling(t_window/2),
+                                 nrow(newdata), 
                                  by = t_window),]
   }
   
-  return(data_infer)
+  return(newdata)
 }
