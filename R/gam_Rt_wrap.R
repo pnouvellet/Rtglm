b6b1c34f7a2ece9149838a993687938446cff125
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-07-12 14:44:29 +0100

    Update gam_Rt_wrap.R

--- R/gam_Rt_wrap.R
+++ R/gam_Rt_wrap.R
@@ -13,8 +13,9 @@ gam_Rt_wrap <- function(I_incid, si_distr, dist = 'poisson'){
   # coefficient in the above is equivalent to logI = log(Rt)+log(OI) -> Rt = exp(coeff)
   k_basis <- 5
   k_check <- 0
-  while((k_check<1) + (k_basis<(nrow(data_infer)-1)) == 2){
-    k_basis <- min(c(k_basis*2,nrow(data_infer)-1))
+  # while((k_check<1) + (k_basis<(nrow(data_infer)-1)) == 2){
+  while((k_check>0.05) + (k_basis<(nrow(data_infer)-1)) == 2){
+      k_basis <- min(c(k_basis*2,nrow(data_infer)-1))
     if(dist == 'nb'){
       m_gam <- mgcv::gam(incidence ~ 0 + s(t, k=k_basis) + offset(log_Oi), 
                          data = data_infer, family = mgcv::nb(link = "log"))
@@ -22,7 +23,8 @@ gam_Rt_wrap <- function(I_incid, si_distr, dist = 'poisson'){
       m_gam <- mgcv::gam(incidence ~ 0 + s(t, k=k_basis) + offset(log_Oi), 
                            data = data_infer, family = poisson(link = "log"))
     }
-    k_check <- mgcv::k.check(m_gam)[3]
+    # k_check <- mgcv::k.check(m_gam)[3]
+    k_check <- mgcv::k.check(m_gam)[4]
     
   }
   mgcv::k.check(m_gam)
