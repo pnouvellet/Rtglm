558e60ffd2a449fbf80c18bc3b4e979dbb4c0fe9
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-07-18 20:21:55 +0100

    Update gam_Rt_sp_wrap.R

--- R/gam_Rt_sp_wrap.R
+++ R/gam_Rt_sp_wrap.R
@@ -14,21 +14,21 @@ gam_Rt_sp_wrap <- function(I_incid, si_distr, x, y){
   m_gam <- mgcv::gam(incidence ~ 0 + s(x,y) + offset(log_Oi), 
                      data = data_infer, family = poisson(link = "log"))
   k_check <- mgcv::k.check(m_gam)[4]
-  if(k_check<0.05){
-    k_basis <- 5
-    k_check <- 0
-    while((k_check<0.05) + (k_basis<(nrow(data_infer)-1)) == 2){
-      k_basis <- min(c(k_basis*2,nrow(data_infer)-1))
-      # coefficient in the above is equivalent to logI = log(Rt)+log(OI) -> Rt = exp(coeff)
-      m_gam <- mgcv::gam(incidence ~ 0 + s(x,y, k=k_basis) + offset(log_Oi), 
-                         data = data_infer, family = poisson(link = "log"))
-      k_check <- mgcv::k.check(m_gam)[4]
-    }
-  }
-  
-  mgcv::k.check(m_gam)
-  summary(m_gam)
-  plot(m_gam)
+  # if(k_check<0.05){
+  #   k_basis <- 5
+  #   k_check <- 0
+  #   while((k_check<0.05) + (k_basis<(nrow(data_infer)-1)) == 2){
+  #     k_basis <- min(c(k_basis*2,nrow(data_infer)-1))
+  #     # coefficient in the above is equivalent to logI = log(Rt)+log(OI) -> Rt = exp(coeff)
+  #     m_gam <- mgcv::gam(incidence ~ 0 + s(x,y, k=k_basis) + offset(log_Oi), 
+  #                        data = data_infer, family = poisson(link = "log"))
+  #     k_check <- mgcv::k.check(m_gam)[4]
+  #   }
+  # }
+  # 
+  # mgcv::k.check(m_gam)
+  # summary(m_gam)
+  # plot(m_gam)
  
   data_infer <- pred_Rtglm(model = m_gam, 
                            newdata = data_infer)
