93235e8ef83f7798a953d75f33a098250b403395
pnouvellet <p.nouvellet@imperial.ac.uk>
2025-02-06 15:25:08 +0000

    doc

--- R/prep_glm.R
+++ R/prep_glm.R
@@ -1,11 +1,26 @@
-#' Hello World
+#' Prepare input for Rt.glm
 #'
-#' This is an example of how to create and document exported functions.
+#' This function construct a dataframe including the time (time-step used in the input), 
+#' incidence (local + imported, see below), and the overall infectivity (calculated as
+#' the sum of past incidence weighted by the serial interval distribution).
 #'
-#' @param input you should always document the paramters.
-#'              Including the expected data type.
+#' @param Incidence A dataframe of non-negative integers with either i) \code{incid$I}
+#'              containing the total incidence, or ii) two columns, so that
+#'              \code{incid$local} contains the incidence of cases due to local transmission
+#'              and \code{incid$imported} contains the incidence of imported cases (with
+#'              \code{incid$local + incid$imported} the total incidence).
+#'
+#' @param si_distr  a vector or dataframe (depending on the method) containing
+#'              the discrete serial interval distribution(s) used for estimation. 
+#'              This is equivalent to the input in EpiEstim when using the "non_parametric_si"
+#'              method.
+#'
+#' @return data_infer a dataframe including the time (t), 
+#'              incidence (incidence), and the overall infectivity (Oi)
 #'
 #' @export
+#' 
+#' 
 prep_glm <- function(I_incid, si_distr){
   # reframe data
   data_infer <- data.frame(t = seq(1,nrow(I_incid)), 
