c5bd30ca8b6be6bd14a95b2676045f4436c048ba
pnouvellet <p.nouvellet@imperial.ac.uk>
2025-07-15 15:52:28 +0100

    Delete Rtglm.R
    form vignette folder

--- vignettes/Rtglm.R
+++
@@ -1,6 +0,0 @@
-## ----install, eval=FALSE------------------------------------------------------
-#  install.packages("Rtglm")
-
-## ----install2, eval=FALSE-----------------------------------------------------
-#  devtools::install_github("reconhub/Rtglm")
-
